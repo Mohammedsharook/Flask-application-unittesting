66b82b198777c9424da4d910ef5e3780b2716844
ayden <aydensharu@gmail.com>
2021-07-02 13:34:06 +0530

    july 2

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, jsonify, request
+from flask import Flask
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:9944394985@localhost/new'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("MYSQL_CONFIG")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-class EmployeeModel(db.Model):
-    __tablename__ = "ayden"
+class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    name = db.Column(db.String(100))
+    Name = db.Column(db.String(200))
+    job = db.relationship('JobRole', backref='user', uselist=False)
+
+
+class JobRole(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    job = db.Column(db.String(200))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
--- main.py
+++ main.py
@@ -1,78 +1,69 @@
-from app import *
-from email_validator import validate_email, EmailNotValidError
+from app import app, User, JobRole, db
+from flask import jsonify, request
 
 
+@app.before_first_request
 @app.route("/")
-def create_table():
+def home():
+    db.create_all()
     return "<h1>Home Page</h1>"
 
 
 @app.route("/detail/<int:user_id>")  # Read
 def details(user_id):
-    try:
-        user = EmployeeModel.query.get(user_id)
-        return jsonify({"Name": user.name, "Email": user.email})
-    except AttributeError:
-        return jsonify('NO USER FOUND AT ID {}'.format(user_id))
+    user = User.query.filter_by(id=user_id).first()
+    job = JobRole.query.filter_by(user_id=user_id).first()
+    if user:
+        return jsonify({"name": user.Name, "job_role": job.job}  )
 
 
-@app.route('/login', methods=['POST'])     # create
+@app.route('/login', methods=['POST'])
 def login():
     income_data = request.get_json()
-    try:
-        if 'name' and 'email' in income_data:
-            name = income_data['name']
-            email = income_data['email']
-            if not name.strip():
-                return jsonify({"Error": {"name": "name field is Not be empty"}})
-            if validate_email(email):
-                user = EmployeeModel(name=name, email=email)
-                db.session.add(user)
-                db.session.commit()
-                return jsonify("Name and Email added successfully")
-        else:
-            return jsonify("Both 'name' and 'email' field is required")
-
-    except EmailNotValidError:
-        return jsonify("Enter valid email address like - {}".format('example@gmail.com'))
+    if 'name' and 'job_role' in income_data:
+        name = income_data['name']
+        job_role = income_data['job_role']
+        if not name.strip() or not job_role.strip():
+            return jsonify({"Error": "name or job_role field is Not be empty"})
+        user = User(Name=name)
+        job = JobRole(job=job_role, user=user)
+        db.session.add(user)
+        db.session.add(job)
+        db.session.commit()
+        return jsonify("Name and job_role added successfully")
+    else:
+        return jsonify("Both 'name' and 'job_role' field is required")
 
 
 @app.route('/update/<int:id>', methods=["PUT"])
 def update(id):
-    employee = EmployeeModel.query.filter_by(id=id).first()
+    user = User.query.filter_by(id=id).first()
     if request.method == 'PUT':
-        if employee:
+        if user:
             income_data = request.get_json()
-            if 'name' and 'email' in income_data:
+            if 'name' and 'job_role' in income_data:
                 name = income_data['name']
-                email = income_data['email']
-                try:
-                    if not name.strip():
-                        return jsonify({"Error": {"name": "name field is Not be empty"}})
-                    validate_email(email)
-                    employee.name = name
-                    employee.email = email
-                    db.session.commit()
-                    return jsonify("User updated successfully")
-                except EmailNotValidError:
-                    return jsonify("Enter valid email address like - {}".format('example@gmail.com'))
+                job_role = income_data['job_role']
+                job = JobRole(job=job_role,user=user)
+                JobRole.job = job
+                user.Name = name
+                db.session.commit()
+                return jsonify("User Name and job_role is updated successfully")
 
-            else:
-                return jsonify("Both 'name' and 'email' field is required to update the user.")
-
+            return jsonify("Both Name and Job_role  field is required to update the user.")
         return jsonify("User id not exists")
 
 
 @app.route('/delete/<int:id>', methods=['delete'])    # delete
 def delete(id):
-    employee = EmployeeModel.query.filter_by(id=id).first()
-    if request.method == 'DELETE':
-        if employee:
-            db.session.delete(employee)
-            db.session.commit()
-            return jsonify("User deleted successfully")
-        else:
-            return jsonify("User ID not exists")
+    if request.method == "DELETE":
+        user = User.query.filter_by(id=id).first()
+        if request.method == "DELETE":
+            if user:
+                db.session.delete(user)
+                db.session.commit()
+                return jsonify("User deleted successfully")
+            return jsonify("User Id Not Exists")
 
 
 if __name__ == '__main__':
